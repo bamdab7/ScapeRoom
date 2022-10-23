911bae4e676729067c28354df11cbcc248bc3fc8
nekoo <nereapena42gmail.com>
2022-10-23 16:40:30 +0200

    Generar numero aleatorio y comprobar si esta cerca o no cambiando el color del feedbacak

--- src/main/java/com/example/scaperoomnpc/HelloApplication.java
+++ src/main/java/com/example/scaperoomnpc/HelloApplication.java
@@ -6,10 +6,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class HelloApplication extends Application {
+    static int numeroAleatorio;
     @Override
     public void start(Stage stage) throws IOException {
+
+        //  AL ARRANCAR GENERAR UN NUMERO RANDOM
+        int max= 100;
+        int min = 1;
+        //  CREATE ISNTANCE OF RANDOM CLASS
+        Random randomNum = new Random();
+        numeroAleatorio= min + randomNum.nextInt(max);
+        System.out.println(numeroAleatorio);
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 588, 350);
         stage.setTitle("Hello!");
--- src/main/java/com/example/scaperoomnpc/HelloController.java
+++ src/main/java/com/example/scaperoomnpc/HelloController.java
@@ -5,18 +5,39 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+import java.util.Random;
+
+import static java.lang.Integer.parseInt;
 
 public class HelloController {
 
-    public TextField nRandom;
-    public TextField intentosTotal;
-    public TextField intentosRestantes;
+    public TextField TXTintentosTotal;
+    public TextField TXTintentosRestantes;
     public TextField numeroUsuario;
-    public Button btnFeedback;
+    public Text TXTFeedBack;
 
-    public void btnFeedback(ActionEvent actionEvent) {
-    }
+//  PARA LLAMAR AL NUMERO ALEATORIO = HelloApplication.numeroAleatorio
+
 
     public void btnProbar(ActionEvent actionEvent) {
+        // ESTE ES EL BOTON QUE HACE LAS COMPROBACIONES
+        int numeroUsuarioInput= parseInt(numeroUsuario.getText());
+            //diferencia
+            int lejos = numeroUsuarioInput - HelloApplication.numeroAleatorio;
+            int lejos2 = HelloApplication.numeroAleatorio - numeroUsuarioInput;
+
+        if(lejos > 20 || lejos2 > 20){
+            TXTFeedBack.setFill(Color.BLUE);
+            System.out.println("El numero esta lejos, diferencia de mas de 20");
+        }else if(lejos < 20 && lejos > 10 || lejos2< 20 && lejos2 >10){
+            TXTFeedBack.setFill(Color.ORANGE);
+            System.out.println("EL numero esta algo cerca, diferencia menos de  20 y  mas de 10");
+        }else if(lejos < 10 || lejos2<10 ){
+            TXTFeedBack.setFill(Color.RED);
+            System.out.println("El numero esta muy cerca, menos de 10");
+        }
     }
 }
