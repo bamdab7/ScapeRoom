866f5d4368f1610ff094a26cb67d9ab48636f494
nekoo <nereapena42gmail.com>
2022-12-06 01:43:31 +0100

    Manejo de error de lanzamiento de scene

--- src/main/java/com/example/scaperoomnpc/HelloApplication.java
+++ src/main/java/com/example/scaperoomnpc/HelloApplication.java
@@ -11,7 +11,7 @@ import java.util.Random;
 public class HelloApplication extends Application {
    // static int numeroAleatorio;
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
         //  AL ARRANCAR GENERAR UN NUMERO RANDOM
 //        int max= 100;
 //        int min = 1;
@@ -26,6 +26,7 @@ public class HelloApplication extends Application {
             scene = new Scene(fxmlLoader.load(), 517, 381);
         }catch (IOException e){
             System.err.println("Error al cargar la escena");
+            System.out.println(e.getMessage());
         }
         stage.setTitle("Scape Room");
         stage.setScene(scene);
