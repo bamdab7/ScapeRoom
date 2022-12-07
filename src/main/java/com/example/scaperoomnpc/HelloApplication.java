b59d461d30c19f82f92ed12c6dddffc4157d0b47
nekoo <nereapena42gmail.com>
2022-12-08 00:57:15 +0100

    Elimando comentarios

--- src/main/java/com/example/scaperoomnpc/HelloApplication.java
+++ src/main/java/com/example/scaperoomnpc/HelloApplication.java
@@ -12,13 +12,6 @@ public class HelloApplication extends Application {
    // static int numeroAleatorio;
     @Override
     public void start(Stage stage) {
-        //  AL ARRANCAR GENERAR UN NUMERO RANDOM
-//        int max= 100;
-//        int min = 1;
-//        //  CREATE ISNTANCE OF RANDOM CLASS
-//        Random randomNum = new Random();
-//        numeroAleatorio= min + randomNum.nextInt(max);
-//        System.out.println(numeroAleatorio);
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = null;
